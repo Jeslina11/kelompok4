f3940f820fe7780ae591864f3fdca96ef344168b
Jeslina11 <jeslinakondo03@gmail.com>
2024-05-28 12:42:47 +0800

    halaman-login

--- login.php
+++ login.php
@@ -1,4 +1,18 @@
 <?php
+
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "kel4"; 
+
+// Membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
 session_start();
 
 function redirectTo($page) {
